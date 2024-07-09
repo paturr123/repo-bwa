4c857b6cabb07282e39480224b7a66b28b1882b8
paturr123 <khafialalf@gmail.com>
2024-07-09 22:16:21 +0800

    menambahkan file bwa

---
+++ bwa-jasa/database/migrations/2024_07_09_073139_create_tagline_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tagline', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('service_id')->nullable()->index('fk_tagline_to_users');
+            $table->string('advantage');
+            $table->string('tagline');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tagline');
+    }
+};
